daee3f7eb8ec5ace1aaf4e7fbe29aaf6bfcf1609
長谷川貴大 <hasegawatakahiro@hasegawatakashidainoMacBook-Pro.local>
2025-07-13 18:57:34 +0900

    Add ECS cluster and ECS security group

--- infra/main.tf
+++ infra/main.tf
@@ -111,3 +111,32 @@ resource "aws_route_table_association" "public_1c" {
   subnet_id      = aws_subnet.public_1c.id
   route_table_id = aws_route_table.public.id
 }
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs-sg"
+  description = "Allow HTTP from ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecs-sg"
+  }
+}
+resource "aws_ecs_cluster" "main" {
+  name = "portfolio-ecs-cluster"
+  tags = {
+    Name = "portfolio-ecs-cluster"
+  }
+}
