5a9268f40ae7dda3e89a071c2f70f597ba05dbad
長谷川貴大 <hasegawatakahiro@hasegawatakashidainoMacBook-Pro.local>
2025-07-13 17:40:52 +0900

    Add VPC and public subnet

---
+++ infra/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.6.0"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "portfolio-vpc"
+  }
+}
+resource "aws_subnet" "public_1a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1a"
+  }
+}
