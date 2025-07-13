24caf33e0c91b42b09ccab42b8238712f3a38ec6
長谷川貴大 <hasegawatakahiro@hasegawatakashidainoMacBook-Pro.local>
2025-07-13 19:01:03 +0900

    Add ALB target group and listener

--- infra/main.tf
+++ infra/main.tf
@@ -140,3 +140,35 @@ resource "aws_ecs_cluster" "main" {
     Name = "portfolio-ecs-cluster"
   }
 }
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "portfolio-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "portfolio-tg"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
